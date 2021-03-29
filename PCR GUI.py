7db26753316e6853b5a522fd9237ab4d73f02e2e
yanami85 <kosaku.1207@gmail.com>
2021-03-29 15:34:49 +0900

    GUI化

---
+++ PCR GUI.py
@@ -0,0 +1,70 @@
+# coding: utf-8
+# PCR_recipe.pyと同じディレクトリにこのファイルを置く
+from  pcr_recipe import pcr_recipe
+import PySimpleGUI as sg
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# レイアウト設計
+layout_input =[
+    [sg.Text("primer foward (5' → 3')",size=(20,1)), sg.InputText("GGGGGGGGGGGGGGG", key = 'primer_fw')],
+    [sg.Text("primer reverse (5' → 3')",size=(20,1)), sg.InputText("GGGGGGGGGGGGGG", key = 'primer_rv')],
+    [sg.Text("プライマー濃度 (μM)",size=(20,1)), sg.InputText("10", key = 'primer_conc_μM')],
+    [sg.Text("増幅する領域",size=(20,1)), sg.InputText("ATGCATGCATGCATGCATGCAaaaaaaaaaaaaaaaaaaaaaa", key = 'amplify_region')],
+    [sg.Text("テンプレート濃度 (ng/μL)",size=(20,1)), sg.InputText("1", key = 'template_conc_ng_μL')],
+    [sg.Text("使うメーカー",size=(20,1)), sg.Combo(["KOD", "PrimeSTAR"])],
+    [sg.Text("サンプルの本数",size=(20,1)), sg.InputText("1", key = 'sample_size')],
+    [sg.Text("反応総量 (μL)",size=(20,1)), sg.InputText("25", key = 'total_vol_μL_per_sample')],
+    [sg.Submit(button_text = "Cancel"), sg.Submit(button_text = "次へ")]
+    ]
+# 窓を作る
+window = sg.Window('PCR レシピ', layout_input, resizable = True)
+
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event in (sg.WIN_CLOSED, 'Cancel'):
+        break
+    if event == "次へ":
+        break
+window.close()
+
+# import pcr_recipe内で定義した変数を入力
+pcr = pcr_recipe()
+pcr.primer_fw = values["primer_fw"]
+pcr.primer_rv = values["primer_rv"]
+pcr.amplify_region = values["amplify_region"]
+pcr.reagent_name = values[0]
+pcr.template_conc_ng_μL = int(values["template_conc_ng_μL"])
+pcr.primer_conc_μM = int(values["primer_conc_μM"])
+pcr.sample_size = int(values["sample_size"])
+pcr.total_vol_μL_per_sample = int(values["total_vol_μL_per_sample"])
+
+# import pcr_recipe内で定義した関数を実行
+pcr.create_pcr_recipe()
+
+layout_tables = [
+        [sg.Text("Thermal cycler",size=(20,1))],
+        [sg.Table(
+            key='-TABLE-',
+            values = pcr.thermal_list,
+            headings = pcr.thermal_col_name
+            )],
+        [sg.Text(str(pcr.reagent_name),size=(20,1))],
+        [sg.Table(
+            key='-TABLE-',
+            values = pcr.conc_table_list,
+            headings = pcr.conc_col_name
+            )],
+        [sg.Button("Exit")],
+    ]
+
+window = sg.Window('pcr_recipe', layout_tables, resizable = True)
+
+
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event in (sg.WIN_CLOSED, 'Exit'):
+        break
+window.close()
---
+++ pcr_recipe.py
@@ -0,0 +1,93 @@
+# coding: utf-8
+import dataclasses
+from dataclasses import dataclass
+from Bio.Alphabet import IUPAC
+from Bio.Seq import Seq
+from Bio import SeqIO
+from Bio.SeqUtils import MeltingTemp as mt
+import pandas as pd
+import numpy as np
+
+@dataclass #required python >= 3.7
+class pcr_recipe: #インスタンス
+    primer_fw: str = "" # primerの塩基配列
+    primer_rv: str = ""
+    amplify_region: str = "" # 増幅させる予定の塩基配列
+    reagent_name: str = "" # "KOD" or "PrimeSTAR"
+    template_conc_ng_μL: int = 1 # template DNAの濃度 1ng/μLが初期
+    primer_conc_μM: int = 10 # primer DNAの濃度 10 μMが初期
+    total_vol_μL_per_sample: int = 25 # 1サンプルあたりの反応液総量 25 μLが初期
+    sample_size: int = 5 # サンプルサイズ
+    
+    def calc_tm_value(self):
+        if self.primer_fw == 0 or self.primer_rv == 0:
+            print("error！primerが入力されていません")
+        else:
+            self.primer_fw_seq = Seq(self.primer_fw.upper(), IUPAC.ambiguous_dna)
+            self.primer_rv_seq = Seq(self.primer_rv.upper(), IUPAC.ambiguous_dna)
+            self.tm_value_NN = min(mt.Tm_NN(self.primer_fw_seq), mt.Tm_NN(self.primer_rv_seq)) # 最近接塩基法で計算
+            self.tm_value_GC = min(mt.Tm_GC(self.primer_fw_seq), mt.Tm_GC(self.primer_rv_seq)) # GC法で計算
+    
+    def count_amplify_region(self):
+        self.amplify_region_seq = Seq(self.amplify_region.upper(), IUPAC.ambiguous_dna)
+        self.amplify_regipon_kbp = len(self.amplify_region_seq)/1000
+    
+    def create_thermal_cycle_plan(self):
+        self.calc_tm_value()
+        self.count_amplify_region()
+        if self.reagent_name == "KOD": # KODの時の温度設定
+            self.thermal_list = [
+                ["94°C", "2 min"],
+                ["94°C", "15 sec"],
+                [str(round(self.tm_value_NN - 5)) + "°C", "30 sec"],
+                ["68°C",  str(round(self.amplify_regipon_kbp, 2)) + " min"]
+                ]
+                
+        elif self.reagent_name == "PrimeSTAR": # PrimeSTARの時の温度設定
+            if self.tm_value_GC - 5 >= 55:
+                self.thermal_list = [
+                ["98°C", "10 sec"],
+                ["55°C", "5 sec"],
+                ["72°C", str(round(self.amplify_regipon_kbp, 2)*5) + "sec"],
+                ]
+            else:
+                self.thermal_list = [
+                ["98°C", "10 sec"],
+                ["55°C", "15 sec"],
+                ["72°C", str(round(self.amplify_regipon_kbp, 2)*5) + "sec"],
+                ]
+                self.message_tm =print(
+                    "PCR thermal setting\n" + "98°C: 10 sec\n" + "55℃: 15 sec\n"
+                    + "72°C: " + str(round(self.amplify_regipon_kbp, 2)*5) + " sec"
+                    )
+        self.thermal_col_name = ["PCR temperature","time"]
+        self.thermal_table= pd.DataFrame(self.thermal_list, columns = self.thermal_col_name)
+    
+    def create_pcr_recipe(self): # 雑に作りすぎた。要改善
+        self.create_thermal_cycle_plan()
+        self.conc_col_name = [r"Reagent", r"Usage/sample (μL)", r"Total amount", "Final concentration"]
+        self.sample_amount = self.total_vol_μL_per_sample*self.sample_size*1.2
+        if self.reagent_name == "KOD":
+            self.conc_list = [
+                ["10×PCR Buffer", self.total_vol_μL_per_sample/10, self.sample_amount/10, "1x"],
+                ["25 mM MgSO4 solution", round(self.total_vol_μL_per_sample/16.7, 2), round(self.sample_amount/16.7, 2), "1.5 mM"],
+                [str(self.primer_conc_μM) + " μM Primer_fw", self.total_vol_μL_per_sample/(self.primer_conc_μM/0.3), round(self.sample_amount/(self.primer_conc_μM/0.3), 3), "0.3 μM"],
+                [str(self.primer_conc_μM) + " μM Primer_rv", self.total_vol_μL_per_sample/(self.primer_conc_μM/0.3), round(self.sample_amount/(self.primer_conc_μM/0.3), 3), "0.3 μM"],
+                ["2 mM dNTPs", self.total_vol_μL_per_sample/10, self.sample_amount/10, "0.2 mM"],
+                [str(self.template_conc_ng_μL) + " ng/μL Template DNA", self.total_vol_μL_per_sample/(self.template_conc_ng_μL/0.04), self.sample_amount/(self.template_conc_ng_μL/0.04), "0.04 ng/μL"],
+                ["KOD plus (1U/μL)", self.total_vol_μL_per_sample/50, self.sample_amount/50, "1 U"]
+            ]
+        elif self.reagent_name == "PrimeSTAR":
+            self.conc_list = [
+                ["PrimeSTAR Max Premix (2×)", self.total_vol_μL_per_sample/2, self.sample_amount/2, "1x"],
+                [str(self.primer_conc_μM) + " μM Primer_fw", self.total_vol_μL_per_sample/(self.primer_conc_μM/0.3), round(self.sample_amount/(self.primer_conc_μM/0.3), 3), "0.3 μM"],
+                [str(self.primer_conc_μM) + " μM Primer_rv", self.total_vol_μL_per_sample/(self.primer_conc_μM/0.3), round(self.sample_amount/(self.primer_conc_μM/0.3), 3), "0.3 μM"],
+                [str(self.template_conc_ng_μL) + " ng/μL Template DNA", self.total_vol_μL_per_sample/(self.template_conc_ng_μL/0.04), self.sample_amount/(self.template_conc_ng_μL/0.04), "0.04 ng/μL"],
+            ]
+        self.conc_table= pd.DataFrame(self.conc_list, columns= self.conc_col_name)
+        self.dw_list = ["DW", self.total_vol_μL_per_sample - self.conc_table["Usage/sample (μL)"].sum(), self.sample_amount - self.conc_table["Total amount"].sum(), "-"]
+        self.conc_table_total = ["Total", self.total_vol_μL_per_sample,  self.sample_amount, "-"]
+        self.conc_table.loc[7] = self.dw_list
+        self.conc_table.loc[8] = self.conc_table_total
+        self.conc_table['sample size'] = self.sample_size
+        self.conc_table_list = self.conc_table.values.tolist()
