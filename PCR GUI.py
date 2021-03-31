4f45babfc545b8ddb9e446aefbbeb4a2954f5966
yanami85 <kosaku.1207@gmail.com>
2021-03-31 11:02:08 +0900

    css対応

--- PCR GUI.py
+++ PCR GUI.py
@@ -27,21 +27,21 @@ def bind_html(html_path_list: list) -> str:
         with open(html_path, encoding="utf-8") as f:
             soup_list.append(BeautifulSoup(f.read()))
     pure_bound_html = ''.join([soup.prettify() for soup in soup_list])
-    bound_html = pure_bound_html.replace('<table border="1" class="dataframe">', '<br><table border="1" class="dataframe">')
-    bound_html = bound_html.replace("right", "center")
+    bound_html = pure_bound_html.replace('<table border="1" class="dataframe">', '<br><table class="mystyle">')
+    bound_html = bound_html.replace('<tr style="text-align: right;">', '<tr>')
     return bound_html
 
 
 # レイアウト設計
 layout_input =[
-    [sg.Text("プライマー foward (5' → 3')",size=(20,1)), sg.InputText(key = 'primer_fw')],
-    [sg.Text("プライマー reverse (5' → 3')",size=(20,1)), sg.InputText(key = 'primer_rv')],
-    [sg.Text("プライマー濃度 (μM)",size=(20,1)), sg.InputText("10", key = 'primer_conc_μM')],
-    [sg.Text("増幅する領域",size=(20,1)), sg.InputText("A"*10000, key = 'amplify_region')],
-    [sg.Text("テンプレート濃度 (ng/μL)",size=(20,1)), sg.InputText("1", key = 'template_conc_ng_μL')],
-    [sg.Text("使うメーカー",size=(20,1)), sg.Combo(["KOD -Plus-", "KOD One", "PrimeSTAR"])],
-    [sg.Text("サンプルの本数",size=(20,1)), sg.InputText("1", key = 'sample_size')],
-    [sg.Text("反応総量 (μL)",size=(20,1)), sg.InputText("25", key = 'total_vol_μL_per_sample')],
+    [sg.Text("プライマー foward (5' → 3')",size=(25,1)), sg.InputText(key = 'primer_fw')],
+    [sg.Text("プライマー reverse (5' → 3')",size=(25,1)), sg.InputText(key = 'primer_rv')],
+    [sg.Text("プライマー濃度 (μM)",size=(25,1)), sg.InputText("10", key = 'primer_conc_μM')],
+    [sg.Text("増幅する領域",size=(25,1)), sg.InputText(key = 'amplify_region')],
+    [sg.Text("テンプレート濃度 (ng/μL)",size=(25,1)), sg.InputText("1", key = 'template_conc_ng_μL')],
+    [sg.Text("使うメーカー",size=(25,1)), sg.Combo(["KOD -Plus-", "KOD One", "PrimeSTAR"])],
+    [sg.Text("サンプルの本数",size=(25,1)), sg.InputText("1", key = 'sample_size')],
+    [sg.Text("反応総量 (μL)",size=(25,1)), sg.InputText("25", key = 'total_vol_μL_per_sample')],
     [sg.Submit(button_text = "Cancel"), sg.Submit(button_text = "次へ")]
     ]
 # 窓を作る
@@ -70,24 +70,30 @@ pcr.total_vol_μL_per_sample = int(values["total_vol_μL_per_sample"])
 # import pcr_recipe内で定義した関数を実行
 pcr.create_pcr_recipe()
 
-layout_tables = [
-    [sg.Text("Tm値 (Wallace法): " + str(round(pcr.tm_value_Wallace, 1)) + "°C",size=(50,1))],
-    [sg.Text("Tm値 (GC法): " + str(round(pcr.tm_value_GC, 1)) + "°C",size=(50,1))],
-    [sg.Text("Tm値 (最近接塩基法): " + str(round(pcr.tm_value_NN, 1)) + "°C",size=(50,1))],
-    [sg.Table(
-        values = pcr.thermal_list,
-        headings = pcr.thermal_col_name
-        )],
-    [sg.Text(str(pcr.reagent_name),size=(20,1))],
-    [sg.Table(
-        values = pcr.conc_table_list,
-        headings = pcr.conc_col_name
-        )],
-    [sg.Button("HTMLに出力")],
+column_1 = [
+    [sg.Text("プライマー forward: " + str(pcr.primer_fw_seq), size=(45,1))],
+    [sg.Text("プライマー reverse: " + str(pcr.primer_rv_seq), size=(45,1))],
+    [sg.Text("Tm値 (Wallace法): " + str(round(pcr.tm_value_Wallace, 1)) + "°C",size=(25,1))],
+    [sg.Text("Tm値 (GC法): " + str(round(pcr.tm_value_GC, 1)) + "°C",size=(25,1))],
+    [sg.Text("Tm値 (最近接塩基法): " + str(round(pcr.tm_value_NN, 1)) + "°C",size=(25,1))]
     ]
 
-window = sg.Window('pcr_recipe', layout_tables, resizable = True)
+column_2 = [
+    [sg.Text("Termal cycler設定")],
+    [sg.Table(
+    values = pcr.thermal_list,
+    headings = pcr.thermal_col_name)]
+    ]
 
+layout_tables = [
+    [sg.Column(column_1, justification= "center"), sg.Column(column_2, justification= "center")],
+    [sg.Text("試薬: " + str(pcr.reagent_name))],
+    [sg.Table(
+        values = pcr.conc_table_list,
+        headings = pcr.conc_col_name)],
+    [sg.Button("HTMLに出力")]]
+
+window = sg.Window('PCR レシピ 結果', layout_tables, resizable = True)
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
@@ -102,6 +108,7 @@ while True:
         html_path_list = ["temp/" + str(datetime.date.today()) + "_tm_table.html", "temp/" + str(datetime.date.today()) + "_thermal_cycle.html", "temp/" + str(datetime.date.today()) + "_conc_table.html"]
         bound_html = bind_html(html_path_list) # html結合
         with open(str(datetime.date.today())+".html", mode='w', encoding="utf-8") as f:
+            f.write('<head> <link rel="stylesheet" type="text/css" href="df_style.css"> </head>')
             f.write("<br>" + "プライマー forward: " + str(pcr.primer_fw_seq) + "<br>")
             f.write("プライマー reverse: " + str(pcr.primer_rv_seq) + "<br>")
             f.write(bound_html) # html出力
